3c7edad282f301c6cf8538470ae0c3bb9e82a21a
Val4_98 <valentin.eloy@ensiie.fr>
2020-11-23 22:39:25 +0100

    Ajout de commentaires

--- WS_HotelClient/src/ws/reservation/Client.java
+++ WS_HotelClient/src/ws/reservation/Client.java
@@ -15,21 +15,23 @@ import ws.reservation.ReservationStub.MakeReservationResponse;
 public class Client {
 	
 	public static Scanner choiceS = new Scanner(System.in);
-	
+	//Fonction main qui lance le menu et permet au client de lancer des recherches
 	public static void main(String[] args) throws AxisFault
 	{
 		int choice;
 		ReservationStub stub = new ReservationStub();
 		Console values = System.console();
 		
-		//Set up connection with customer and get his id
+		//On lance la connexion avec les identifiants du client et on obtient son identifiant
 		String customer_id = AuthentCall.makeAuthent();
 		
+		//On propose au client deux choix, chercher les hotels disponibles 1) ou faire une reservation 2)
 		while(true) {
 		System.out.println("\n########## MENU ##########");
 		System.out.println("1) Check hostels availability");
 		System.out.println("2) Make a reservation");
 		
+		//On verifie la pertinence de sa reponse
 		try {
 			choice = Integer.parseInt(choiceS.nextLine());
 		
@@ -51,20 +53,21 @@ public class Client {
 	}
 	}
 	
-	//Method to check if date filled are correct to make a research
+	//Methode qui teste si les dates de debut et de fin saisies sont correctes pour faire la recherche
+	//En fonction du resultat on envoi une reponse positive ou negative
 	private static boolean dateChecking(String start_date, String end_date) { 
 	    Date start = null;
 	    Date end = null;
 	    boolean response = false;
 	    
 			try {
-				//System.out.println(start_date);
-				//System.out.println(end_date);
 				start = new SimpleDateFormat("dd/MM/yyyy").parse(start_date);
 				end = new SimpleDateFormat("dd/MM/yyyy").parse(end_date);
+				//On test si les deux dates ne sont pas nulles
 				if(start != null && end != null) 
 				{
 					int different = start.compareTo(end);
+					//On teste si la date de fin est apres la date de debut
 					if(different > 0 ) {
 						System.out.println("Error in the selected dates, End date must be after Start date");
 						response = false;
@@ -84,15 +87,19 @@ public class Client {
 		
 		return response;
 	}
-	//Method to check if number of place filled is positive and isn't null
+	//Methode qui teste si le nombre de place saisi est un entier positif et non null
+	//En fonction du resultat on envoi une reponse positive ou negative
 	private static boolean placeChecking(String nb_place) {
 		boolean response = false;
+		//On teste si nombre n'est pas null
 		if(nb_place == null || nb_place.equals("")){
 			nb_place="null";
 			response = true;
 		}else{
 			try {
+				//On test si le nombre saisi est un entier
 				int count = Integer.parseInt(nb_place);  
+				//On verifie que ce ne soit pas un entier null
 				if(count <= 0) {
 					System.out.println("Error in number of place(s) choosen it must be at least of 1");
 					response = false;
@@ -101,24 +108,26 @@ public class Client {
 					response = true;
 				}
 			} catch (NumberFormatException e) {
-				// TODO Auto-generated catch block
 				response = false;
 				System.out.println("Number of place(s) must be an integer number");
 			}
 		}
 		return response;
 	}
-	//Method to check if max price filled is positive and isn't null
+	//Methode qui teste si le prix maximum saisi est un entier positif et non null	//En fonction du resultat on envoi une reponse positive ou negative
+	//En fonction du resultat on envoi une reponse positive ou negative
 	private static boolean maxPriceChecking(String max_price) {
 		boolean response = false;
 		int price;
-		
+		//On teste si le prix saisi n'est pas null
 		if(max_price == null || max_price.equals("")){
 			max_price="null";
 			response = true;
 		}else{
 			try {
+				//On teste si le prix saisi est bien un entier
 				price = Integer.parseInt(max_price);
+				//On teste si le prix saisi est bien positif et entier
 				if(price <= 0) {
 					System.out.println("Maximum price can't be negative of null");
 					response = false;
@@ -134,6 +143,8 @@ public class Client {
 		return response;
 	}
 	
+	//Methode pour realiser une reservation, lit en entree les valeurs et les envoies au service reservation 
+	// renvoi une reponse de succes ou d'echec de la reservation
 	public static void MakeReservation(ReservationStub stub, Console values, String customer_id)
 	{
 		ReservationStub.MakeReservation informations = new ReservationStub.MakeReservation();
@@ -142,14 +153,14 @@ public class Client {
 		
 		String room_id,start,end;
 		
-		//makeReservation(String customer_id, String room_id, String start_date, String end_date)
-		
+		//On lit le numero de chambre saisi, si le format est correcte on sort de la boucle.
 		do {
 		System.out.println("Id of the room [needed] : ");
 		room_id = choiceS.nextLine();
 		verification_room_id = room_idChecking(room_id);
 		}while(verification_room_id == false);
 		
+		//On lit les dates de debut et de fin, si les formats sont correctes on sort de la boucle.
 		do {
 			System.out.println("Start date (dd/mm/yyyy) [needed] : ");
 			start = choiceS.nextLine();
@@ -158,14 +169,17 @@ public class Client {
 			verification_dates = dateChecking(start, end);
 		} while(verification_dates == false);
 		
+		//On converti le format des dates dd/mm/yyyy au format yyyy-mm-dd
 		start = convertionTime(start);
 		end = convertionTime(end);
 		
+		//On passe les parametres au service reservation
 		informations.setCustomer_id(customer_id);
 		informations.setRoom_id(room_id);
 		informations.setStart_date(start);
 		informations.setEnd_date(end);
 		
+		//On recupere la reponse qui est directement formulee par le service reservation
 		MakeReservationResponse response;
 		try {
 			response = stub.makeReservation(informations);
@@ -176,6 +190,10 @@ public class Client {
 			System.out.println("Connection error to Reservation Webservice");	
 		}
 	}
+	
+	//Methode pour realiser une recherche de chambre d'hotel, lit en entree les valeurs et les envoies au service reservation 
+	// renvoi une liste de chambre hotel correspondant aux critères.
+	// Si un champ non obligatoire n'est pas saisi, on prend toutes les valeurs repondant a ce critere
 	public static void listHotel(ReservationStub stub, Console values)
 	{
 		ReservationStub.ListHotel informations = new ReservationStub.ListHotel();
@@ -186,27 +204,34 @@ public class Client {
 		boolean verification_location = false;
 		
 		String start, end, room_count,max_price, location;
-		//listHotel(String max_price, String nb_place,String location,String start_date, String end_date)
 		
+		//On lit le prix maximum saisi, si le format est correcte on sort de la boucle
+		//Cette saisie n'est pas obligatoire
 		do {
 			System.out.println("Max room price : ");
 			max_price = choiceS.nextLine();
 			verification_prices = maxPriceChecking(max_price);
 		} while(verification_prices == false);
 		
+		//On lit le nombre de chambre saisi, si le format est correcte on sort de la boucle
+		//Cette saisie n'est pas obligatoire
 		do {
 			System.out.println("Number of rooms : ");
 			room_count = choiceS.nextLine();
 			verification_places = placeChecking(room_count);
 		} while(verification_places == false);
 		
+		//On lit la ville saisie, si le format est correcte on sort de la boucle
+		//Cette saisie n'est pas obligatoire
 		do {
 		System.out.println("Asked city : ");
 		location = choiceS.nextLine();
 		verification_location = locationChecking(location);
 		}while(verification_location == false);
 
-		do { //NE VEUT PAS BOUCLER
+		//On lit les dates de debut et de fin, si les formats sont correctes on sort de la boucle
+		//Ces informations sont obligatoires
+		do {
 			System.out.println("Start date (dd/mm/yyyy) [needed] : ");
 			start = choiceS.nextLine();
 			System.out.println("End date (dd/mm/yyyy) [needed] : ");
@@ -214,10 +239,11 @@ public class Client {
 			verification_dates = dateChecking(start, end);
 		} while(verification_dates == false);
 		
+		//On converti le format des dates dd/mm/yyyy au format yyyy-mm-dd
 		start = convertionTime(start);
 		end = convertionTime(end);
 		
-		//ajout par lucas
+		//Si les valeurs saisie sont vides, on les définies sur null
 		if(max_price == null || max_price.equals("")){
 			max_price="null";
 		}
@@ -227,39 +253,47 @@ public class Client {
 		if(location == null || location.equals("")){
 			location="null";
 		}
+		//On passe les parametres au service reservation
 		informations.setMax_price(max_price);
 		informations.setNb_place(room_count);
 		informations.setLocation(location);
 		informations.setStart_date(start);
 		informations.setEnd_date(end);
 		
+		//Partie  affichage des resultats
+		//On recupere les resultats sous forme d'une chaine de caractere
+		//Chaque hotel a 10 parametres, on met en forme les resultats dans un tableau
 		System.out.println("\n########## START RESEARCH ##########");
 		ReservationStub.ListHotelResponse response;
 		try {
 			response = stub.listHotel(informations);
 			String result = response.get_return();
 			if(result.equals("False")) {
+				//Si il n'y a pas de resultat repondant aux criteres on affiche ce message
 				System.out.println("Sorry, we haven't found a room for you according to your criteria\n\nI'm sure you will find an other room !");
 			}
 			else {
+				//On met en forme le tableau a partir de la chaine de caractere
 				int nb_room = ((result.split(";")).length)/10;
 				System.out.println("\nThere is "+nb_room+" room founded : \n");
 				String [] datas = result.split(";");
+				//On ajoute la legende de chaque colonne
 				System.out.println("       HOTEL                      CITY             BED(S)   ID ROOM  PRICE(EUR)   PHONE NUMBER        WEBSITE\n");
+				//Pour mettre en forme le tableau on recupere chaque hotel en decoupant la chaine
 				for(int i=0; i<nb_room;i++) {
 					for(int y=0;y<10;y++) {
+						//On choisit les valeurs que l'on veut afficher
 						int [] size = {0, 2, 4, 5, 6, 3, 1};
+						//On parcours le tableau d'hotel a hotel
 						int indice = y + (i*10);
-
+						//Si on tombe sur le nom de l'hotel on affiche tout le reste
 						if(y == 1) {
 							for(int z = 0; z < 5; z++) {
 								int lenght;
+								//Pour avoir un tableau avec une forme qui se tient on regarde la longueur de la chaine et on ajoute des espaces pour completer
 								int [] lenghtTab = { 5, 20 };
 								if(z == 2 || z == 3 || z == 4 || z == 5) {
 									lenght = lenghtTab[0];
-									/*if( z == 4) {
-										datas[indice+size[z]] = datas[indice+size[z]]+"€";
-									}*/
 								}
 								else {
 									lenght = lenghtTab[1];
@@ -277,6 +311,7 @@ public class Client {
 							for(int s=0; s<line.length();s++) {
 								separation = separation + "~";
 							}
+							//On ajoute des separations entre chaque lignes
 							System.out.println(separation);
 							System.out.println(line);
 							if(i == nb_room-1) {
@@ -287,6 +322,7 @@ public class Client {
 					}
 				}
 			}
+			//On termine l'affichage des resultats, on informe le client de comment reserver une chambre
 			System.out.println("\n######################################");
 			System.out.println("\nYou can reserve a room by using it ID ROOM in '2) Make a reservation' Menu.");
 			
@@ -295,10 +331,12 @@ public class Client {
 		}
 	}
 	
+	//Methode qui teste si le numero de la chambre saisi est un entier
 	private static boolean room_idChecking(String room_id) {
 		boolean response = false;
 		
 		try {
+			//On test si c'est un entier
 			int id = Integer.parseInt(room_id);
 			response = true;
 		} catch (NumberFormatException e) {
@@ -307,9 +345,12 @@ public class Client {
 		}
 		return response;
 	}
+	//Methode qui teste si la vile saisie est une chaine de caracteres
 	private static boolean locationChecking(String location) {
 		boolean response = false;
+		//On test que la ville ne soit pas nulle avant de tester son type
 		if(location != null) {
+			//On teste son type en verifiant qu'il ne contient pas de chiffre
 			if(location.matches(".*\\d.*")== true) {
 				response = false;
 				System.out.println("The location must not be null or contain a number");
@@ -321,15 +362,13 @@ public class Client {
 		return response;
 	}
 
-	//Method to convert date format asked to database format needed
+	//Methode qui convertit les dates au format voulu
 	private static String convertionTime(String time) {
 		String day = time.substring(0, 2);
 		String month = time.substring(3, 5);
 		String year = time.substring(6, 10);
 		String newDate = year+"-"+month+"-"+day;
 		
-		//System.out.println("Old date :"+time);
-		//System.out.println("New date :"+newDate);
 		return newDate;
 	}
 }
--- WS_HotelReservation/src/ws/reservation/Reservation.java
+++ WS_HotelReservation/src/ws/reservation/Reservation.java
@@ -9,7 +9,7 @@ import filterinteraction.ClientCall;
 
 public class Reservation {
 	public String listHotel(String max_price, String nb_place,String location,String start_date, String end_date){
-		//check inputs
+		//On verifie les entrees
 		System.out.println("listHOTEL");
 		if(max_price==null){
 			max_price="null";
@@ -24,16 +24,15 @@ public class Reservation {
 			start_date="null";
 			end_date="null";
 		}
-		//call filter ws
+		//On appelle filter WS et on recupere la reponse contenant la liste des chambres d'hotel
 		String response = ClientCall.filterQuery(max_price, nb_place, location, start_date, end_date);
-		//format response
 		
-		//return
+		//On envoi la reponse au client
 		return response;
 	}
 	
 	public String makeReservation(String customer_id, String room_id, String start_date, String end_date){
-		//check inputs
+		//On verifie la conformite des entrees
 		if(customer_id == null){
 			return "User problem.";
 		}
@@ -49,19 +48,20 @@ public class Reservation {
 		String start_param = "&start_date=" + start_date;
 		String end_param = "&end_date=" + end_date;
 		
-		//call filter WS
+		//On appelle le filtre WS
 		String response = ClientCall.reservationQuery(customer_id_param,
 													room_id_param,
 													start_param,
 													end_param);
 		
-		//reformate response
+		//Si filter WS nous repond vrai on envoi la reponse positive au client
 		if( response.equals("True")){
 			response = "Reservation successful for the room "+room_id;
+		//Si filter WS nous repond faux on envoi la reponse negative au client
 		}else{
 			response = "Reservation error, the room "+ room_id +" is not available";
 		}
-		//resturn response
+		//On envoie la reponse formulee
 		return response;
 	}
 }
